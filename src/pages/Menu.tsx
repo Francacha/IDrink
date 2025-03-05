89b525e8d851dc1a2d252e2f3f4ffeaec4791480
Franco Arce <francacha10@gmail.com>
2025-03-05 11:08:31 -0300

    Clear

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,13 @@
+export default function Menu() {
+    return (
+       <header className="bg-slate-800 h-32"> 
+        <div className="mx-auto container px-5 h-full flex items-center">
+            <div className="flex justify-between items-center w-full">
+                <div>
+                    <img className="w-48" src="/IDrinksinfond.png" alt="logotipo" /> 
+                </div>
+            </div>
+        </div>
+       </header>
+    );
+}
