b4c0aec1aed10165c68dd288804977e280479258
Franco Arce <francacha10@gmail.com>
2025-03-12 18:02:21 -0300

    outwelcome

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,15 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from './layouts/Layout'
 const IndexPage = lazy(() => import('./pages/IndexPage'))
 const FavoritesPage = lazy(() => import('./pages/FavoritesPage'))
 const AppPage = lazy(() => import('./pages/AppPage'));
 const HowUse = lazy(() => import('./pages/HowUse'));
-const WelcomePage = lazy(() => import('./pages/WelcomePage'));
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
         <Routes>
-        <Route path='/welcome' element={
-          <Suspense fallback="Cargando...">
-            <WelcomePage />
-          </Suspense>
-        } />
-        <Route path='/' element={<Navigate to="/welcome" />} /> {/* 👈 Redirige al inicio siempre */}
            <Route element={<Layout />}>
               <Route path='/' element={
                   <Suspense fallback="Cargando...">
