2b09bca1a9b0b75dfe8303854c4564011996255d
Franco Arce <francacha10@gmail.com>
2025-03-12 18:22:27 -0300

    sup

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -33,7 +33,7 @@ export default function IndexPage() {
 
     return (
         <>
-            <h1 className="text-6xl font-extrabold">Recetas</h1>
+            <h1 className="text-6xl text-slate-800 font-extrabold">Tragos & Recetas</h1>
 
             {hasDrinks ? (
                 <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
